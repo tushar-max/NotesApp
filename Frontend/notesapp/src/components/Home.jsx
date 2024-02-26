86b0e8a9a8202049def9886d32e1bdc374d8adff
Tushar <tushar@code.com>
2024-02-26 23:16:27 +0530

    removing env

--- Frontend/notesapp/src/components/Home.jsx
+++ Frontend/notesapp/src/components/Home.jsx
@@ -2,13 +2,23 @@ import React, { useEffect, useState } from "react";
 import MediaCard from "./Card";
 import "./Home.css";
 import axios from "axios";
+import { Fab } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 const Home = () => {
-  // const temp = [{_id: '65db3ef8f17f8a1bf2d46fc2', name: 'Tushar Awasthi', description: 'Testing 1234', __v: 0}, {_id: '65dcb93a9d86f441beb5a780', name: 'What is Lorem Ipsum?', description: 'Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.', __v: 0}];
+  const temp = [{_id: '65db3ef8f17f8a1bf2d46fc2', name: 'Tushar Awasthi', description: 'Testing 1234', __v: 0}, {_id: '65dcb93a9d86f441beb5a780', name: 'What is Lorem Ipsum?', description: 'Lorem Ipsum is simply dummy text of the printing a…ldus PageMaker including versions of Lorem Ipsum.', __v: 0}];
   const [notes, setNotes] = useState([]);
-  const cards = Array.from({ length: notes.length}, (_, index) => (
+  const cards = Array.from({ length: notes.length+10 }, (_, index) => (
     <div className="width">
-      <MediaCard color="neutral" key={index} orientation="horizontal" size="lg" variant="soft" className="element" data={notes[index]}/>
+      <MediaCard
+        color="neutral"
+        key={index}
+        orientation="horizontal"
+        size="lg"
+        variant="soft"
+        className="element"
+        data={temp[index%2]}
+      />
     </div>
   ));
 
@@ -25,7 +35,16 @@ const Home = () => {
     fetchData();
   }, []);
 
-  return <div className="row container">{cards}</div>;
+  return (
+    <>
+      <div className="row container">{cards}</div>
+      <div className="bottom-right">
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      </div>
+    </>
+  );
 };
 
 export default Home;
